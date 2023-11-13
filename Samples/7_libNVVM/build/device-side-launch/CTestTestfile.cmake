5d771d735fb71e96ad1161cd7493a464f7f22cd9
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2023-11-13 18:58:18 +0100

    Remove binary

--- Samples/7_libNVVM/build/device-side-launch/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tojas/cuda-samples/Samples/7_libNVVM/device-side-launch
-# Build directory: /home/tojas/cuda-samples/Samples/7_libNVVM/build/device-side-launch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(device-side-launch "/home/tojas/cuda-samples/Samples/7_libNVVM/build/device-side-launch/dsl")
-set_tests_properties(device-side-launch PROPERTIES  WORKING_DIRECTORY "/home/tojas/cuda-samples/Samples/7_libNVVM/build" _BACKTRACE_TRIPLES "/home/tojas/cuda-samples/Samples/7_libNVVM/device-side-launch/CMakeLists.txt;35;add_test;/home/tojas/cuda-samples/Samples/7_libNVVM/device-side-launch/CMakeLists.txt;0;")
--- Samples/7_libNVVM/build/uvmlite/CMakeFiles/clobber.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clobber.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
